7c536ee487a8a0192da16b39ebb2f2192101317d
punzie <c_a_mcginty@yahoo.com>
2018-08-08 14:08:34 -0400

    Production url
    -Updated production url for deployment

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
+  production: 'https://thawing-anchorage-62811.herokuapp.com/',
   development: 'http://localhost:4741'
 }
 
