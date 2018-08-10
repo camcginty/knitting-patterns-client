a6f1169bc23288add5a00f5e1e299f132ac224c9
punzie <c_a_mcginty@yahoo.com>
2018-08-10 08:31:08 -0400

    Fix deployed url
    -Remove extra / in deployed url

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,7 +2,7 @@
 
 let apiUrl
 const apiUrls = {
-  production: 'https://thawing-anchorage-62811.herokuapp.com/',
+  production: 'https://thawing-anchorage-62811.herokuapp.com',
   development: 'http://localhost:4741'
 }
 
